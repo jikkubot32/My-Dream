7a471bdcba841ce4e0902efcddf5e3a8dc5c0377
jikkubot32 <93936137+jikkubot32@users.noreply.github.com>
2022-05-14 16:15:52 +0400

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -37,7 +37,7 @@ async def save_group(bot, message):
             await bot.leave_chat(message.chat.id)
             return
         buttons = [[
-            InlineKeyboardButton('Support', url=f'https://t.me/{SUPPORT_CHAT}')
+            InlineKeyboardButton('📢 Updates', url='https://t.me/FilmClub_Channel')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
