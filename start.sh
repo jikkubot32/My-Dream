252732d6b76ce1fb5d121498ffddb54f00a1affb
jikkubot32 <93936137+jikkubot32@users.noreply.github.com>
2023-04-09 04:39:19 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/jikkubot32/My-Dream.git /My-Dream
+  git clone https://github.com/jikkubot32/My-Dream.git /DQ-Join
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /My-Dream
 fi
 cd /My-Dream
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "Starting My-Dream...."
 python3 bot.py
